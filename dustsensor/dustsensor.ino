f87ce41b880f5c250d9ef90f3198ac841b86a9df
nagyeomlee <kb.nglee@gmail.com>
2021-04-12 01:56:31 +0900

    최종코드
    자동모드 동작 시 창문여닫는 수치범위 수정

--- dustsensor/dustsensor.ino
+++ dustsensor/dustsensor.ino
@@ -1,27 +1,29 @@
-#include <Stepper.h>
-#include <SoftwareSerial.h>
-#include <LiquidCrystal_I2C.h>
-SoftwareSerial BTSerial(3,4);
-LiquidCrystal_I2C lcd(0x3F, 16, 2);
+#include <Stepper.h>          // 스테핑모터 사용을 위한 헤더파일
+#include <SoftwareSerial.h>  // 블루투스 사용을 위한 헤더파일
+#include <LiquidCrystal_I2C.h>      // I2C LCD 사용을 위한 헤더파일
+SoftwareSerial BTSerial(3,4);       // 블루투스 핀으로 TX = 3, RX = 4 설정
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // I2C LCD 핀 설정, 주소, 16칸X2줄
 
-// 스텝모터에서 스텝을 조절, 100 = 180도, 200 = 360도
+// 스텝모터에서 스텝을 조절
+// 100 = 180도, 200 = 360도, 300 = 540도, 400 = 720도
 const int stepsPerRevolution = 100;
 
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11); // 스텝(현재 100), 모터랑 연결된 핀 8, 9, 10, 11
 
-#define BLUE 5
-#define GREEN 6
-#define RED 7
+#define BLUE 5  // 5번핀 => BLUE
+#define GREEN 6 // 6번핀 => GREEN
+#define RED 7   // 7번핀 => RED
 
-int Vo = A0;
-int V_LED = 2;
+int Vo = A0;   // 미세먼지 센서의 아날로그 값을 받는 Vo = 아두이노 UNO의 Analog Pin 0번
+int V_LED = 2; // 미세먼지 센서에 IRED (적외선 LED) = Digital Pin 2번
 
+// float 형식의 변수들을 0으로 값 초기화
 float Vo_value = 0;
 float Voltage = 0;
 float dustDensity = 0;
 
-int cnt;
-int window;
+int cnt1, cnt2;
+int window = 0;
 
 int redValue = 0;
 int greenValue = 0;
@@ -32,9 +34,9 @@ char data = 'a';
 
 void setup()
 {
-  myStepper.setSpeed(60);
-  Serial.begin(115200);
-  BTSerial.begin(115200);
+  myStepper.setSpeed(60);   // 스텝모터 스피드 셋팅 (60)
+  Serial.begin(115200);     // 시리얼 모니터 셋팅 (속도 : 115200 bps)
+  BTSerial.begin(115200);   // Bluetooth인 JMOD-BT-1의 속도가 115200bps라서 속도를 하나로 맞춰야 함
   pinMode(V_LED, OUTPUT);
   pinMode(Vo, INPUT);
   lcd.begin();
@@ -46,7 +48,7 @@ void setup()
   pinMode(BLUE, OUTPUT);
   digitalWrite(RED, LOW);
   digitalWrite(GREEN, LOW);
-  digitalWrite(BLUE, HIGH);
+  digitalWrite(BLUE, LOW);
 }
 
 void loop()
@@ -54,7 +56,7 @@ void loop()
   if (BTSerial.available())
     temp = BTSerial.read();
 
-  digitalWrite(V_LED, LOW);
+  digitalWrite(V_LED, LOW); // PNP 트랜지스터 때문에 LOW가 LED 켜짐상태
   delayMicroseconds(280);
   Vo_value = analogRead(Vo);
   delayMicroseconds(40);
@@ -62,7 +64,8 @@ void loop()
   delayMicroseconds(9680);
   
   Voltage = Vo_value * 5.0 / 1024.0;
-  dustDensity = (Voltage - 0.6)/0.005,1;
+  dustDensity = (Voltage - 0.6)/0.005;
+  dustDensity = roundf(dustDensity * 100) / 100;
   String dustString = String (dustDensity);
 
   if ( dustDensity < 0 )
@@ -71,12 +74,12 @@ void loop()
     return;
   }
 
-  Serial.print(" Voltage : ");
+  Serial.print("Voltage : ");
   Serial.println(Voltage);
-  Serial.print(" Dust Density : ");
+  Serial.print("Dust Density : ");
   Serial.println(dustDensity);
 
-  BTSerial.println(dustString);
+  BTSerial.print(dustString);
 
   lcd.setCursor(0, 1);
   lcd.print("dust : ");
@@ -104,77 +107,84 @@ void Mode (char temp)
 
     if (data == 'm')
     {
-      switch(temp)
+      if (temp == '1' && window == 0) // 수동 창문 열기
       {
-        case '1' : // 문 열기
-        break;
-        case '2' : // 문 닫기
-        break;
-        default : break;
+        Serial.println("manual window open");
+        myStepper.step(stepsPerRevolution);
+        window = 1;
+        delay(1000);
+      }
+      if (temp == '2' && window == 1) // 수동 창문 닫음
+      {
+        Serial.println("manual window close");
+        myStepper.step(-stepsPerRevolution);
+        window = 0;
+        delay(1000);
       }
     }
 
     else if (data == 'a')
     {
-      if (dustDensity < 30)
+      if (dustDensity < 35) // 35미만, 좋음, RGB = 파랑
       {
-        greenValue = 20;
-        blueValue = 255;
-        redValue = 0;
+        redValue = 255;
+        greenValue = 255;
+        blueValue = 0;
         analogWrite(GREEN, greenValue);
         analogWrite(BLUE, blueValue);
         analogWrite(RED, redValue);
+        cnt2++;
         }
-        else if (dustDensity >= 30 && dustDensity < 80)
+        else if (dustDensity >= 35 && dustDensity < 75) // 35이상 75미만, 조금 나쁨, RGB = 노랑
         {
-          greenValue = 255;
-          blueValue = 20;
-          redValue = 0;
+          redValue = 80;
+          greenValue = 80;
+          blueValue = 255;
           analogWrite(RED, redValue);
           analogWrite(GREEN, greenValue);
           analogWrite(BLUE, blueValue);
-          cnt++;
+          cnt1++;
          }
-         else if (dustDensity >= 80 && dustDensity < 150)
+         else if (dustDensity >= 75) // 75 이상, 나쁨, RGB = 빨강
          {
-          redValue = 250;
-          greenValue = 60;
-          blueValue = 0;
+          redValue = 0;
+          greenValue = 255;
+          blueValue = 255;
           analogWrite(RED, redValue);
           analogWrite(GREEN, greenValue);
           analogWrite(BLUE, blueValue);
-          cnt++;
+          cnt1++;
          }
-          else if (dustDensity > 150)
-          {
-          redValue = 220;
-          greenValue = 0;
-          blueValue = 0;
-          analogWrite(RED, redValue);
-          analogWrite(GREEN, greenValue);
-          analogWrite(BLUE, blueValue);
-          cnt++;
-          }
 
-          Serial.print("현재 cnt 값 : ");
-          Serial.println(cnt);
+          Serial.print("BAD dustDensity count : ");
+          Serial.println(cnt1);
+          Serial.print("GOOD dustDensity count : ");
+          Serial.println(cnt2);
 
-          if (dustDensity >= 30)
+          if (cnt2 == 10 && window == 0) // 0 = 닫힘, 1 = 열림
           {
-            Serial.println("open window.");
+            Serial.println("auto window.open");
             myStepper.step(stepsPerRevolution);
             window = 1;
+            cnt1 = 0;
+            cnt2 = 0;
             delay(5000);
           }
 
-          if (dustDensity < 30 && window == 1)
+          if (cnt1 == 10 && window == 1)
           {
-            Serial.println("close window.");
+            Serial.println("auto window close");
             myStepper.step(-stepsPerRevolution);
             window = 0;
+            cnt1 = 0;
+            cnt2 = 0;
             delay(5000);
           }
-          
+
+          if (cnt1 == 10 || cnt2 == 10)
+          {
+            cnt1 = 0;
+            cnt2 = 0;
+          }
     }
 }
-​
